2bef233d87bb8d6ad7ce182c4c021b256c24f7fd
Rick Barenthin <rb@terralink.de>
2017-05-23 14:26:11 +0200

    style fixes und cleanup

--- core/rtp/telephone_event.h
+++ core/rtp/telephone_event.h
@@ -18,8 +18,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
@@ -27,8 +27,8 @@
  * telephone_event.h  --  RTP Payload for DTMF Digits (RFC 2833)
  */
 
-#ifndef _telephone_event_h_
-#define _telephone_event_h_
+#ifndef _TELEPHONE_EVENT_H_
+#define _TELEPHONE_EVENT_H_
 
 #include <sys/types.h>
 
@@ -36,28 +36,28 @@
  * The type definitions below are valid for 32-bit architectures and
  * may have to be adjusted for 16- or 64-bit architectures.
  */
-typedef unsigned char  u_int8;
+typedef unsigned char u_int8;
 typedef unsigned short u_int16;
-typedef unsigned int   u_int32;
+typedef unsigned int u_int32;
 
 /*
  * DTMF Payload
  */
-typedef struct {
-    
-    u_int8  event;
+typedef struct
+{
+  u_int8 event;
 
 #if (defined(__BYTE_ORDER) && (__BYTE_ORDER == __BIG_ENDIAN))
-    u_int8  e:1;
-    u_int8  r:1;
-    u_int8  volume:6;
+  u_int8 e : 1;
+  u_int8 r : 1;
+  u_int8 volume : 6;
 #else
-    u_int8  volume:6;
-    u_int8  r:1;
-    u_int8  e:1;
+  u_int8 volume : 6;
+  u_int8 r : 1;
+  u_int8 e : 1;
 #endif
 
-    u_int16 duration;
+  u_int16 duration;
 
 } dtmf_payload_t;
 
