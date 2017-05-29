c0c2898fd6b370f52872309506a6db87051c283c
Rick Barenthin <rb@terralink.de>
2017-05-29 23:48:04 +0200

    style fixes und cleanup

--- core/AmSession.h
+++ core/AmSession.h
@@ -20,78 +20,81 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
+/** @file AmSession.h */
 
-#ifndef _AmSession_h_
-#define _AmSession_h_
+#ifndef _AMSESSION_H_
+#define _AMSESSION_H_
 
-#include "AmRtpStream.h"
-#include "AmThread.h"
-#include "AmEventQueue.h"
-#include "AmRtpAudio.h"
+#include "AmApi.h"
 #include "AmDtmfDetector.h"
-#include "AmSipMsg.h"
-#include "AmSipHeaders.h"
+#include "AmEventQueue.h"
+#include "AmMediaProcessor.h"
+#include "AmRtpAudio.h"
+#include "AmRtpStream.h"
+#include "AmSessionEventHandler.h"
 #include "AmSipDialog.h"
 #include "AmSipEvent.h"
-#include "AmApi.h"
-#include "AmSessionEventHandler.h"
-#include "AmMediaProcessor.h"
+#include "AmSipHeaders.h"
+#include "AmSipMsg.h"
+#include "AmThread.h"
 
 #include "AmZRTP.h"
 
+#include <map>
+#include <queue>
 #include <string>
 #include <vector>
-#include <queue>
-#include <map>
+
 using std::string;
 using std::vector;
+using std::unique_ptr;
 
 class AmSessionFactory;
 class AmDtmfEvent;
 
-/** @file AmSession.h */
-
 /* definition imported from Ser parser/msg_parser.h */
 #define FL_FORCE_ACTIVE 2
 
-
 /**
  * \brief Implements the default behavior of one session
- * 
+ *
  * The session is identified by Call-ID, From-Tag and To-Tag.
  */
-class AmSession : 
-  public virtual AmObject,
+class AmSession
+    : public virtual AmObject
+    ,
 #ifndef SESSION_THREADPOOL
-  public AmThread,
+      public AmThread
+    ,
 #endif
-  public AmEventQueue, 
-  public AmEventHandler,
-  public AmSipDialogEventHandler,
-  public AmMediaSession,
-  public AmDtmfSink
+      public AmEventQueue
+    , public AmEventHandler
+    , public AmSipDialogEventHandler
+    , public AmMediaSession
+    , public AmDtmfSink
 {
-  AmMutex      audio_mut;
+  AmMutex audio_mut;
 
-protected:
-  vector<SdpPayload *>  m_payloads;
-  //bool         negotiate_onreply;
+ protected:
+  vector<SdpPayload*> m_payloads;
+  // bool         negotiate_onreply;
 
   friend class AmRtpAudio;
 
   /** get new RTP format for the session */
-  //virtual AmAudioRtpFormat* getNewRtpFormat();
+  // virtual AmAudioRtpFormat* getNewRtpFormat();
 
-private:
+ private:
   AmDtmfDetector   m_dtmfDetector;
   AmDtmfEventQueue m_dtmfEventQueue;
-  bool m_dtmfDetectionEnabled;
+  bool             m_dtmfDetectionEnabled;
 
-  enum ProcessingStatus { 
+  enum ProcessingStatus
+  {
     SESSION_PROCESSING_EVENTS = 0,
     SESSION_WAITING_DISCONNECTED,
     SESSION_ENDED_DISCONNECTED
@@ -103,23 +106,23 @@ private:
   void run();
   void on_stop();
 #else
-public:
+ public:
   void start();
   bool is_stopped();
 
-private:
-  void stop();
+ private:
+  void  stop();
   void* _pid;
 #endif
 
   static void session_started();
   static void session_stopped();
 
-  static volatile unsigned int session_num;
-  static volatile unsigned int session_count;
-  static volatile unsigned int max_session_num;
+  static volatile unsigned int       session_num;
+  static volatile unsigned int       session_count;
+  static volatile unsigned int       max_session_num;
   static volatile unsigned long long avg_session_num;
-  static AmMutex session_num_mut;
+  static AmMutex                     session_num_mut;
 
   friend class AmMediaProcessor;
   friend class AmMediaProcessorThread;
@@ -127,19 +130,18 @@ private:
   friend class AmSessionFactory;
   friend class AmSessionProcessorThread;
 
-  std::unique_ptr<AmRtpAudio> _rtp_str;
+  unique_ptr<AmRtpAudio> _rtp_str;
 
   /** Application parameters passed through P-App-Param HF */
-  map<string,string> app_params;
+  map<string, string> app_params;
 
   /** Sets the application parameters from the original request */
   void setAppParams(const AmSipRequest& req);
 
-protected:
-
+ protected:
   AmCondition<bool> sess_stopped;
 
-  /** this is the group the media is processed with 
+  /** this is the group the media is processed with
       - by default local tag */
   string callgroup;
 
@@ -156,7 +158,7 @@ protected:
 
   virtual AmSipDialog* createSipDialog();
 
-  /** process pending events,  
+  /** process pending events,
       @return whether everything went smoothly */
   virtual bool processEventsCatchExceptions();
 
@@ -169,12 +171,12 @@ protected:
   /** clean up session */
   void finalize();
 
-public:
-
-  enum SessionRefreshMethod {
-    REFRESH_REINVITE = 0,      // use reinvite
-    REFRESH_UPDATE,            // use update
-    REFRESH_UPDATE_FB_REINV    // use update or fallback to reinvite
+ public:
+  enum SessionRefreshMethod
+  {
+    REFRESH_REINVITE = 0,   // use reinvite
+    REFRESH_UPDATE,         // use update
+    REFRESH_UPDATE_FB_REINV // use update or fallback to reinvite
   };
   /** currently selected session refresh method */
   SessionRefreshMethod refresh_method;
@@ -183,34 +185,39 @@ public:
   void updateRefreshMethod(const string& headers);
 
   AmRtpAudio* RTPStream();
-  bool hasRtpStream() { return _rtp_str.get() != NULL; }
+  bool        hasRtpStream() { return _rtp_str.get() != NULL; }
 
 #ifdef WITH_ZRTP
   AmZRTPSessionState zrtp_session_state;
 
   /** must be set before session is started! i.e. in constructor */
   bool enable_zrtp;
-
 #endif
 
   AmSipDialog* dlg;
 
-  /** 
+  /**
    * \brief Exception occured in a Session
-   * 
+   *
    * Session (creation) should be aborted and replied with code/reason.
    */
-  struct Exception {
-    int code;
+  struct Exception
+  {
+    int    code;
     string reason;
     string hdrs;
-    Exception(int c, string r, string h="") : code(c), reason(r), hdrs(h) {}
+    Exception(int c, string r, string h = "")
+        : code(c)
+        , reason(r)
+        , hdrs(h)
+    {
+    }
   };
 
-  /** 
+  /**
    * Session constructor.
    */
-  AmSession(AmSipDialog* dlg=NULL);
+  AmSession(AmSipDialog* dlg = NULL);
 
   virtual ~AmSession();
 
@@ -220,9 +227,9 @@ public:
   virtual void process(AmEvent*);
 
   /**
-   * add a handler which will be called 
+   * add a handler which will be called
    * for all events in session
-   * 
+   *
    * @see AmSessionEventHandler
    */
   void addHandler(AmSessionEventHandler*);
@@ -238,8 +245,8 @@ public:
   /**
    * Set the call group for this call; calls in the same
    * group are processed by the same media processor thread.
-   * 
-   * Note: this must be set before inserting 
+   *
+   * Note: this must be set before inserting
    * the session to the MediaProcessor!
    */
   void setCallgroup(const string& cg);
@@ -303,7 +310,10 @@ public:
   void setReceiving(bool receive) { RTPStream()->setReceiving(receive); }
 
   /** setter for rtp_str->force_receive_dtmf*/
-  void setForceDtmfReceiving(bool receive) { RTPStream()->force_receive_dtmf = receive; }
+  void setForceDtmfReceiving(bool receive)
+  {
+    RTPStream()->force_receive_dtmf = receive;
+  }
 
   /* ----         SIP dialog attributes                  ---- */
 
@@ -311,7 +321,7 @@ public:
   const string& getCallID() const;
 
   /** Gets the Session's remote tag */
-  const string& getRemoteTag()const ;
+  const string& getRemoteTag() const;
 
   /** Gets the Session's local tag */
   const string& getLocalTag() const;
@@ -342,11 +352,11 @@ public:
   virtual bool refresh(int flags = 0);
 
   /** send an UPDATE in the session */
-  virtual int sendUpdate(const AmMimeBody* body, const string &hdrs);
+  virtual int sendUpdate(const AmMimeBody* body, const string& hdrs);
 
   /** send a Re-INVITE (if connected) */
   virtual int sendReinvite(bool updateSDP = true, const string& headers = "",
-			   int flags = 0);
+                           int flags = 0);
 
   /** send an INVITE */
   virtual int sendInvite(const string& headers = "");
@@ -357,7 +367,9 @@ public:
   /** update UAC trans state reference from old_cseq to new_cseq
       e.g. if uac_auth or session_timer have resent a UAC request
    */
-  virtual void updateUACTransCSeq(unsigned int old_cseq, unsigned int new_cseq) { }
+  virtual void updateUACTransCSeq(unsigned int old_cseq, unsigned int new_cseq)
+  {
+  }
 
   /* ----         Householding                              ---- */
 
@@ -376,9 +388,9 @@ public:
 
   /**
    * Signals the session it should stop.
-   * This will cause the session to be able 
+   * This will cause the session to be able
    * to exit the main loop.
-   * If wakeup is set, a bogus event will 
+   * If wakeup is set, a bogus event will
    * be sent to wake up the session.
    */
   virtual void setStopped(bool wakeup = false);
@@ -410,12 +422,13 @@ public:
   /**
    * Entry point for DTMF events
    */
-  void postDtmfEvent(AmDtmfEvent *);
+  void postDtmfEvent(AmDtmfEvent*);
 
   void setInbandDetector(Dtmf::InbandDetectorType t);
   bool isDtmfDetectionEnabled() { return m_dtmfDetectionEnabled; }
   void setDtmfDetectionEnabled(bool e) { m_dtmfDetectionEnabled = e; }
-  void putDtmfAudio(const unsigned char *buf, int size, unsigned long long system_ts);
+  void putDtmfAudio(const unsigned char* buf, int size,
+                    unsigned long long system_ts);
 
   /**
    * send a DTMF as RTP payload (RFC4733)
@@ -449,7 +462,7 @@ public:
   /**
      remove all Timers
      @return true on success
-     Note: this doesn't clear timer events already in the 
+     Note: this doesn't clear timer events already in the
            event queue
   */
   virtual bool removeTimers();
@@ -477,33 +490,33 @@ public:
   virtual void onInvite(const AmSipRequest& req);
 
   /**
-   * onOutgoingInvite will be called if an INVITE 
+   * onOutgoingInvite will be called if an INVITE
    * is sent in the session.
    */
-  virtual void onOutgoingInvite(const string& headers) { }
+  virtual void onOutgoingInvite(const string& headers) {}
 
   /**
    * onCancel will be called if a CANCEL for a running
    * dialog has been received. At this point, the CANCEL
    * transaction has been replied with 200.
    *
-   * A normal plug-in does not have to do anything special, 
-   * as normal dialogs are immediatly replied with 200 
-   * or error code. 
+   * A normal plug-in does not have to do anything special,
+   * as normal dialogs are immediatly replied with 200
+   * or error code.
    *
-   * Note: You are still responsible for responding the 
+   * Note: You are still responsible for responding the
    *       initial transaction.
    */
   virtual void onCancel(const AmSipRequest& req);
 
   /**
-   * onRinging will be called after 180 is received. 
+   * onRinging will be called after 180 is received.
    * If local audio is set up, session is added to media processor.
    */
   virtual void onRinging(const AmSipReply& reply) {}
 
   /**
-   * onBye is called whenever a BYE request is received. 
+   * onBye is called whenever a BYE request is received.
    */
   virtual void onBye(const AmSipRequest& req);
 
@@ -514,21 +527,21 @@ public:
   virtual void onSipRequest(const AmSipRequest& req);
 
   /** Entry point for SIP Replies   */
-  virtual void onSipReply(const AmSipRequest& req, const AmSipReply& reply, 
-			  AmBasicSipDialog::Status old_dlg_status);
+  virtual void onSipReply(const AmSipRequest& req, const AmSipReply& reply,
+                          AmBasicSipDialog::Status old_dlg_status);
 
   /** 2xx reply has been received for an INVITE transaction */
   virtual void onInvite2xx(const AmSipReply& reply);
 
-  virtual void onInvite1xxRel(const AmSipReply &);
+  virtual void onInvite1xxRel(const AmSipReply&);
 
   /** answer for a locally sent PRACK is received */
-  virtual void onPrack2xx(const AmSipReply &);
+  virtual void onPrack2xx(const AmSipReply&);
 
   virtual void onFailure();
-  
+
   virtual void onNoAck(unsigned int cseq);
-  virtual void onNoPrack(const AmSipRequest &req, const AmSipReply &rpl);
+  virtual void onNoPrack(const AmSipRequest& req, const AmSipReply& rpl);
 
   /**
    * Entry point for Audio events
@@ -539,13 +552,15 @@ public:
    * entry point for system events
    */
   virtual void onSystemEvent(AmSystemEvent* ev);
-  
+
 #ifdef WITH_ZRTP
   /**
    * ZRTP events @see ZRTP
    */
-  virtual void onZRTPProtocolEvent(zrtp_protocol_event_t event, zrtp_stream_t *stream_ctx);
-  virtual void onZRTPSecurityEvent(zrtp_security_event_t event, zrtp_stream_t *stream_ctx);
+  virtual void onZRTPProtocolEvent(zrtp_protocol_event_t event,
+                                   zrtp_stream_t*        stream_ctx);
+  virtual void onZRTPSecurityEvent(zrtp_security_event_t event,
+                                   zrtp_stream_t*        stream_ctx);
 #endif
 
   /** This callback is called if RTP timeout encountered */
@@ -559,7 +574,8 @@ public:
   virtual void onSendRequest(AmSipRequest& req, int& flags);
 
   /** Called by AmSipDialog when a reply is sent */
-  virtual void onSendReply(const AmSipRequest& req, AmSipReply& reply, int& flags);
+  virtual void onSendReply(const AmSipRequest& req, AmSipReply& reply,
+                           int& flags);
 
   /** Hook called when an SDP offer is required */
   virtual bool getSdpOffer(AmSdp& offer);
@@ -570,17 +586,19 @@ public:
   /** Hook called when an SDP OA transaction has been completed */
   virtual int onSdpCompleted(const AmSdp& offer, const AmSdp& answer);
 
-  /** Hook called when an early session starts (SDP OA completed + dialog in early state) */
+  /** Hook called when an early session starts (SDP OA completed + dialog in
+   * early state) */
   virtual void onEarlySessionStart();
 
-  /** Hook called when the session creation is completed (INV trans replied with 200) */
+  /** Hook called when the session creation is completed (INV trans replied with
+   * 200) */
   virtual void onSessionStart();
 
-  /** 
+  /**
    * called in the session thread before the session is destroyed,
    * i.e. after the main event loop has finished
    */
-  virtual void onBeforeDestroy() { }
+  virtual void onBeforeDestroy() {}
 
   // The IP address to put as c= in SDP bodies
   string advertisedIP(int addrType = AT_NONE);
@@ -598,42 +616,35 @@ public:
 
   /* ----------------- media processing interface ------------------- */
 
-public: 
-  virtual int readStreams(unsigned long long ts, unsigned char *buffer);
-  virtual int writeStreams(unsigned long long ts, unsigned char *buffer);
+ public:
+  virtual int readStreams(unsigned long long ts, unsigned char* buffer);
+  virtual int writeStreams(unsigned long long ts, unsigned char* buffer);
   virtual void clearRTPTimeout() { RTPStream()->clearRTPTimeout(); }
   virtual void processDtmfEvents();
 
   /**
    * Call-backs used by RTP stream(s)
-   * 
+   *
    * Note: these methods will be called from the RTP receiver thread.
    */
   virtual bool onBeforeRTPRelay(AmRtpPacket* p, sockaddr_storage* remote_addr)
-  { return true; }
+  {
+    return true;
+  }
 
   virtual void onAfterRTPRelay(AmRtpPacket* p, sockaddr_storage* remote_addr) {}
 
-  int getRtpInterface();
+  int  getRtpInterface();
   void setRtpInterface(int _rtp_interface);
 };
 
-inline AmRtpAudio* AmSession::RTPStream() {
+inline AmRtpAudio* AmSession::RTPStream()
+{
   if (NULL == _rtp_str.get()) {
-    DBG("creating RTP stream instance for session [%p]\n", 
-	this);
-    _rtp_str.reset(new AmRtpAudio(this,rtp_interface));
+    DBG("creating RTP stream instance for session [%p]\n", this);
+    _rtp_str.reset(new AmRtpAudio(this, rtp_interface));
   }
   return _rtp_str.get();
 }
 
-
 #endif
-
-/** EMACS **
- * Local variables:
- * mode: c++
- * c-basic-offset: 2
- * End:
- */
-
