661a9d605c71fbd8f7433d58ac63ffc5e25d6264
Rick Barenthin <dunst0@gmail.com>
2017-07-10 15:41:02 +0200

    more fixes now to apps

--- apps/ann_b2b/AnnounceB2B.cpp
+++ apps/ann_b2b/AnnounceB2B.cpp
@@ -18,84 +18,85 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 #include "AnnounceB2B.h"
-#include "AmSessionContainer.h"
-#include "AmConfig.h"
-#include "AmUtils.h"
+
 #include "AmApi.h"
-
+#include "AmConfig.h"
 #include "AmMediaProcessor.h"
-
-#include "sems.h"
+#include "AmSessionContainer.h"
+#include "AmUtils.h"
 #include "log.h"
+#include "sems.h"
+
+using std::string;
+using std::map;
 
 #define MOD_NAME "ann_b2b"
 
-EXPORT_SESSION_FACTORY(AnnounceB2BFactory,MOD_NAME);
+EXPORT_SESSION_FACTORY(AnnounceB2BFactory, MOD_NAME);
 
 string AnnounceB2BFactory::AnnouncePath;
 string AnnounceB2BFactory::AnnounceFile;
 
 AnnounceB2BFactory::AnnounceB2BFactory(const string& _app_name)
-  : AmSessionFactory(_app_name)
+    : AmSessionFactory(_app_name)
 {
 }
 
 int AnnounceB2BFactory::onLoad()
 {
   AmConfigReader cfg;
-  if(cfg.loadFile(AmConfig::ModConfigPath + string(MOD_NAME ".conf")))
+  if (cfg.loadFile(AmConfig::ModConfigPath + string(MOD_NAME ".conf")))
     return -1;
 
   // get application specific global parameters
   configureModule(cfg);
 
-  AnnouncePath = cfg.getParameter("announce_path",ANNOUNCE_PATH);
-  if( !AnnouncePath.empty() 
-      && AnnouncePath[AnnouncePath.length()-1] != '/' )
+  AnnouncePath = cfg.getParameter("announce_path", ANNOUNCE_PATH);
+  if (!AnnouncePath.empty() && AnnouncePath[AnnouncePath.length() - 1] != '/')
     AnnouncePath += "/";
 
-  AnnounceFile = cfg.getParameter("default_announce",ANNOUNCE_FILE);
-  DBG("AnnounceFile = %s\n",AnnounceFile.c_str());
+  AnnounceFile = cfg.getParameter("default_announce", ANNOUNCE_FILE);
+  DBG("AnnounceFile = %s\n", AnnounceFile.c_str());
 
   string announce_file = AnnouncePath + AnnounceFile;
-  if(!file_exists(announce_file)){
+  if (!file_exists(announce_file)) {
     ERROR("default file for ann_b2b module does not exist ('%s').\n",
-	  announce_file.c_str());
+          announce_file.c_str());
     return -1;
   }
 
   return 0;
 }
 
-AmSession* AnnounceB2BFactory::onInvite(const AmSipRequest& req, const string& app_name,
-					const map<string,string>& app_params)
+AmSession* AnnounceB2BFactory::onInvite(const AmSipRequest& req,
+                                        const string&       app_name,
+                                        const map<string, string>& app_params)
 {
   string announce_path = AnnouncePath;
-  string announce_file = announce_path + req.domain
-    + "/" + req.user + ".wav";
-    
-  DBG("trying '%s'\n",announce_file.c_str());
-  if(file_exists(announce_file))
+  string announce_file = announce_path + req.domain + "/" + req.user + ".wav";
+
+  DBG("trying '%s'\n", announce_file.c_str());
+  if (file_exists(announce_file))
     return new AnnounceCallerDialog(announce_file);
-    
+
   announce_file = announce_path + req.user + ".wav";
-  DBG("trying '%s'\n",announce_file.c_str());
-  if(file_exists(announce_file))
+  DBG("trying '%s'\n", announce_file.c_str());
+  if (file_exists(announce_file))
     return new AnnounceCallerDialog(announce_file);
-    
+
   announce_file = AnnouncePath + AnnounceFile;
   return new AnnounceCallerDialog(announce_file);
 }
 
 AnnounceCallerDialog::AnnounceCallerDialog(const string& filename)
-  : filename(filename), 
-    AmB2BCallerSession()
+    : filename(filename)
+    , AmB2BCallerSession()
 {
   // we want to answer
   //  the call ourself
@@ -116,9 +117,9 @@ void AnnounceCallerDialog::onSessionStart()
   // this disables DTMF detection as well
   setReceiving(false);
 
-  if(wav_file.open(filename,AmAudioFile::Read))
+  if (wav_file.open(filename, AmAudioFile::Read))
     throw string("AnnouncementDialog::onSessionStart: Cannot open file\n");
-    
+
   setOutput(&wav_file);
 
   AmB2BCallerSession::onSessionStart();
@@ -126,13 +127,10 @@ void AnnounceCallerDialog::onSessionStart()
 
 void AnnounceCallerDialog::process(AmEvent* event)
 {
-
   AmAudioEvent* audio_event = dynamic_cast<AmAudioEvent*>(event);
-  if(audio_event && (audio_event->event_id == AmAudioEvent::cleared)){
+  if (audio_event && (audio_event->event_id == AmAudioEvent::cleared)) {
+    if (getCalleeStatus() != None) return;
 
-    if (getCalleeStatus() != None)
-      return;
-	
     // detach this session from the media
     // because we will stay in signaling only
     AmMediaProcessor::instance()->removeSession(this);
@@ -140,6 +138,6 @@ void AnnounceCallerDialog::process(AmEvent* event)
     connectCallee(callee_addr, callee_uri);
     return;
   }
-    
+
   AmB2BCallerSession::process(event);
 }
--- apps/ann_b2b/AnnounceB2B.h
+++ apps/ann_b2b/AnnounceB2B.h
@@ -18,55 +18,51 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 #ifndef _ANNOUNCEB2B_H_
 #define _ANNOUNCEB2B_H_
 
-#include "AmSession.h"
-#include "AmConfigReader.h"
 #include "AmAudioFile.h"
 #include "AmB2BSession.h"
+#include "AmConfigReader.h"
+#include "AmSession.h"
 
+#include <map>
 #include <string>
-using std::string;
 
 /** \brief Factory for AnnounceB2B sessions */
-class AnnounceB2BFactory: public AmSessionFactory
+class AnnounceB2BFactory : public AmSessionFactory
 {
-public:
-  static string AnnouncePath;
-  static string AnnounceFile;
+ public:
+  static std::string AnnouncePath;
+  static std::string AnnounceFile;
 
-  AnnounceB2BFactory(const string& _app_name);
+  AnnounceB2BFactory(const std::string& _app_name);
 
-  int onLoad();
-  AmSession* onInvite(const AmSipRequest& req, const string& app_name,
-		      const map<string,string>& app_params);
+  int        onLoad();
+  AmSession* onInvite(const AmSipRequest& req, const std::string& app_name,
+                      const std::map<std::string, std::string>&   app_params);
 };
 
 /** \brief Session logic implementation of A leg in announce_b2b sessions */
-class AnnounceCallerDialog: public AmB2BCallerSession
+class AnnounceCallerDialog : public AmB2BCallerSession
 {
   AmAudioFile wav_file;
-  string filename;
+  std::string filename;
 
-  string callee_addr;
-  string callee_uri;
-    
-public:
-  AnnounceCallerDialog(const string& filename);
-    
+  std::string callee_addr;
+  std::string callee_uri;
+
+ public:
+  AnnounceCallerDialog(const std::string& filename);
+
   void process(AmEvent* event);
   void onInvite(const AmSipRequest& req);
   void onSessionStart();
 };
 
 #endif
-// Local Variables:
-// mode:C++
-// End:
-
