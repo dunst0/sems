3013600b3c14309b034c3d4af0ab0f24adbb89db
Rick Barenthin <dunst0@gmail.com>
2017-08-12 21:45:35 +0200

    fix thread stop with parameter

--- core/sip/tcp_trsp.cpp
+++ core/sip/tcp_trsp.cpp
@@ -686,7 +686,7 @@ void tcp_server_socket::start_threads()
 void tcp_server_socket::stop_threads()
 {
   for (unsigned int i = 0; i < workers.size(); i++) {
-    workers[i]->stop(false);
+    workers[i]->stop();
     workers[i]->join();
   }
 }
