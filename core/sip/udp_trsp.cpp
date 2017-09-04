89cfb3ea0c449057ecab88e035dd24515ec5906b
Rick Barenthin <dunst0@gmail.com>
2017-09-04 22:49:46 +0200

    fix

--- core/sip/udp_trsp.cpp
+++ core/sip/udp_trsp.cpp
@@ -388,7 +388,7 @@ void udp_trsp::run()
   }
 
   if (msg.msg_control) {
-    delte (u_char *) msg.msg_control;
+    delete (u_char *) msg.msg_control;
     msg.msg_control = NULL;
     msg.msg_controllen = 0;
   }
